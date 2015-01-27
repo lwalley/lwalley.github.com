1e55b0d327cbda986cf7b95c3d2d54660e63e902
Lisa Walley <lw2596@columbia.edu>
2015-01-27 16:38:20 -0500

    Removed CNAME; updated config.

--- README.md
+++ README.md
@@ -2,19 +2,19 @@ Using ruhoh, sass and compass to build github pages.
 
 During development, preview pages with rackup:
 
-    $ rackup -p 9292
+    $ bundle exec rackup -p 9292
 
 Auto compile stylesheets with compass:
 
-    $ compass watch .
+    $ bundle exec compass watch .
 
 
 For production compile stylesheets with compass:
 
-    $ compass compile -e production -s compressed
+    $ bundle exec compass compile -e production -s compressed
 
 Then compile static pages with rohuh:
 
-    $ ruhoh compile
+    $ bundle exec ruhoh compile
 
 Then move compiled site to master branch.
--- config.rb
+++ config.rb
@@ -3,11 +3,11 @@ require 'zen-grids'
 
 # Set this to the root of your project when deployed:
 http_path       = "/"
-css_dir         = "themes/iamdev/stylesheets"
-sass_dir        = "themes/iamdev/sass"
-images_dir      = "themes/iamdev/media"
-javascripts_dir = "themes/iamdev/javascripts"
-fonts_dir       = "themes/iamdev/fonts"
+css_dir         = "iamdev/stylesheets"
+sass_dir        = "iamdev/sass"
+images_dir      = "iamdev/media"
+javascripts_dir = "iamdev/javascripts"
+fonts_dir       = "iamdev/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
